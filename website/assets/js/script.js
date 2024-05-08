fe9f85b3dd0d08d8ee3129c23707eff81dcade40
MaiaComley <comleymaia@gmail.com>
2024-05-08 19:19:48 +0100

    wednesday

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,4 +1,6 @@
+// Quote Index
 var currentQuote = 0;
+// List of quotes
 var quotes = [
     `"Every bad situation is a blues song waiting to happen."`,
     `"I fall in love every day. Not with people but with situations."`,
@@ -10,10 +12,29 @@ var quotes = [
 ];
 
 function showQuote() {
-    console.log(`Clicked`);
+    // Get random number
     currentQuote = Math.floor(Math.random() * quotes.length);
+    // Get random quote from that number
     var quote = quotes[currentQuote];
+    // Set the quote element text to the quote text
     document.querySelector(`.amy-quote`).textContent = quote;
+    // Scroll the quote element into view smoothly
+    document.querySelector(`.amy-quote`).scrollIntoView({
+        behavior: 'smooth'
+    })
 }
 
+// When we click the quote button we want to trigger the function above 
 document.querySelector(`.amy-quotes-button`).addEventListener(`click`, showQuote);
+
+function fadeIn(entries) {
+    console.log(entries);
+    entries.forEach((entry) => {
+        entry.target.classList.toggle('fade-in-now', true);
+        entry.target.classList.toggle('fade-in', false);
+    });
+}
+
+
+var observer = new IntersectionObserver(fadeIn, { threshold: 1 });
+document.querySelectorAll(`.fade-in`).forEach((element) => observer.observe(element));
